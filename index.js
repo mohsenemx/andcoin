17cb0ee27bbfc4d51c845778d0465e2bb16d54cf
Mohsen Taghavi <78661162+mohsenemx@users.noreply.github.com>
2024-04-29 23:08:38 +0330

    update 2

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 import { WebSocketServer } from 'ws';
-import { fs } from 'fs';
+import * as fs from 'fs';
 const wss = new WebSocketServer({ port: 8080,  });
 console.log('Listening on ws://localhost:8080/connection');
+let users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
-    console.log(data);
+    let parsed = JSON.parse(data);
+    if (parsed.action == 'login') {
+      //console.log('Test');
+      users.forEach(element => {
+        if (element.hash == parsed.hash) {
+          console.log('Users exists, logging in');
+        } else {
+          console.log('User does not exist, creating new user');
+        }
+      });
+    }
+    //console.log(data);
   });
 
   ws.send('{code: 200, message: \'connection established\'}');
+  ws.on('error', console.error);
+});
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping();
+  });
+}, 30000);
+
+wss.on('close', function close() {
+  clearInterval(interval);
 });
