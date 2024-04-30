63c975a3ea958edc4b87c8da4d1b6ebcf423dc83
Mohsen Taghavi <78661162+mohsenemx@users.noreply.github.com>
2024-04-30 21:25:09 +0330

    eh idk update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import * as fs from 'fs';
 const wss = new WebSocketServer({ port: 8080,  });
 console.log('Listening on ws://localhost:8080/connection');
 let users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
+let usersTemplate = JSON.stringify(fs.readFileSync('./data/users.template.json', 'utf8'));
+let saveInterval = setInterval(() => {
+  fs.writeFileSync('./data/users.json', JSON.stringify(users));
+}, 60000);
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     let parsed = JSON.parse(data);
@@ -13,6 +17,16 @@ wss.on('connection', function connection(ws) {
           console.log('Users exists, logging in');
         } else {
           console.log('User does not exist, creating new user');
+          if ((parsed.name != null || parsed.name.trim() != '') && (parsed.hash != null || parsed.hash.trim() != '')) {
+            let newUser = JSON.parse(usersTemplate);
+            
+            /*newUser.name = parsed.name;
+            newUser.hash = parsed.hash;
+            newUser.tgId = parsed.tgId;
+            users.push(newUser);*/
+            ws.send('{ "action" : "createAccount", "result" : "successful" }');
+          }
+          console.log(users);
         }
       });
     }
