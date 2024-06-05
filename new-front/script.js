ab197aff1efb080331fca704c69a1dadd5bc5d01
amir <codamir7814@gmail.com>
2024-06-05 15:06:45 +0430

    boost page & wallet page

--- new-front/script.js
+++ new-front/script.js
@@ -1,9 +1,40 @@
 function Home(){
     document.querySelector("#HomePage").style.display = "block";
     document.querySelector("#TasksPage").style.display = "none";
+    document.querySelector("#TradePage").style.display = "none";
+    document.querySelector("#BoostPage").style.display = "none";
+    document.querySelector("#BoostPage").style.display = "none";
+    document.querySelector("#WalletPage").style.display = "none";
 }
 
 function Tasks(){
     document.querySelector("#HomePage").style.display = "none";
     document.querySelector("#TasksPage").style.display = "block";
+    document.querySelector("#TradePage").style.display = "none";
+    document.querySelector("#BoostPage").style.display = "none";
+    document.querySelector("#Walletpage").style.display = "none";
 }
+
+function Trade(){
+    document.querySelector("#HomePage").style.display = "none";
+    document.querySelector("#TasksPage").style.display = "none";
+    document.querySelector("#TradePage").style.display = "block";
+    document.querySelector("#BoostPage").style.display = "none";
+    document.querySelector("#WalletPage").style.display = "none";
+}
+function Boost(){
+    document.querySelector("#HomePage").style.display = "none";
+    document.querySelector("#TasksPage").style.display = "none";
+    document.querySelector("#TradePage").style.display = "none";
+    document.querySelector("#BoostPage").style.display = "block";
+    document.querySelector("#WalletPage").style.display = "none";
+}
+function Wallet(){
+    document.querySelector("#HomePage").style.display = "none";
+    document.querySelector("#TasksPage").style.display = "none";
+    document.querySelector("#TradePage").style.display = "none";
+    document.querySelector("#BoostPage").style.display = "none";
+    document.querySelector("#WalletPage").style.display = "block";
+}
+
+
