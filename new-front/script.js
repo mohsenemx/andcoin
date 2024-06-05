1968c2e7b3db271af36feb309047f6414234d19a
amir <codamir7814@gmail.com>
2024-06-05 19:33:28 +0430

    js bug

--- new-front/script.js
+++ new-front/script.js
@@ -3,7 +3,6 @@ function Home(){
     document.querySelector("#TasksPage").style.display = "none";
     document.querySelector("#TradePage").style.display = "none";
     document.querySelector("#BoostPage").style.display = "none";
-    document.querySelector("#BoostPage").style.display = "none";
     document.querySelector("#WalletPage").style.display = "none";
 }
 
@@ -12,7 +11,7 @@ function Tasks(){
     document.querySelector("#TasksPage").style.display = "block";
     document.querySelector("#TradePage").style.display = "none";
     document.querySelector("#BoostPage").style.display = "none";
-    document.querySelector("#Walletpage").style.display = "none";
+    document.querySelector("#WalletPage").style.display = "none";
 }
 
 function Trade(){
