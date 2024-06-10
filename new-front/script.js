8ba351403508a93e517cd7fb811ca27fc6973450
amir <codamir7814@gmail.com>
2024-06-11 01:17:37 +0430

    new pages

--- new-front/script.js
+++ new-front/script.js
@@ -1,7 +1,8 @@
 function Home(){
     document.querySelector("#HomePage").style.display = "block";
     document.querySelector("#TasksPage").style.display = "none";
-    document.querySelector("#TradePage").style.display = "none";
+    document.querySelector("#TradePage2").style.display = "none";
+    document.querySelector("#TradePage1").style.display = "none";
     document.querySelector("#BoostPage").style.display = "none";
     document.querySelector("#WalletPage").style.display = "none";
 }
@@ -9,7 +10,8 @@ function Home(){
 function Tasks(){
     document.querySelector("#HomePage").style.display = "none";
     document.querySelector("#TasksPage").style.display = "block";
-    document.querySelector("#TradePage").style.display = "none";
+    document.querySelector("#TradePage2").style.display = "none";
+    document.querySelector("#TradePage1").style.display = "none";
     document.querySelector("#BoostPage").style.display = "none";
     document.querySelector("#WalletPage").style.display = "none";
 }
@@ -17,23 +19,38 @@ function Tasks(){
 function Trade(){
     document.querySelector("#HomePage").style.display = "none";
     document.querySelector("#TasksPage").style.display = "none";
-    document.querySelector("#TradePage").style.display = "block";
+    document.querySelector("#TradePage2").style.display = "block";
+    document.querySelector("#TradePage1").style.display = "none";
     document.querySelector("#BoostPage").style.display = "none";
     document.querySelector("#WalletPage").style.display = "none";
 }
 function Boost(){
     document.querySelector("#HomePage").style.display = "none";
     document.querySelector("#TasksPage").style.display = "none";
-    document.querySelector("#TradePage").style.display = "none";
+    document.querySelector("#TradePage2").style.display = "none";
+    document.querySelector("#TradePage1").style.display = "none";
     document.querySelector("#BoostPage").style.display = "block";
     document.querySelector("#WalletPage").style.display = "none";
 }
 function Wallet(){
     document.querySelector("#HomePage").style.display = "none";
     document.querySelector("#TasksPage").style.display = "none";
-    document.querySelector("#TradePage").style.display = "none";
+    document.querySelector("#TradePage2").style.display = "none";
+    document.querySelector("#TradePage1").style.display = "none";
     document.querySelector("#BoostPage").style.display = "none";
     document.querySelector("#WalletPage").style.display = "block";
+}
+
+
+function TradeETH(){
+    document.querySelector("#HomePage").style.display = "none";
+    document.querySelector("#TasksPage").style.display = "none";
+    document.querySelector("#TradePage1").style.display = "block";
+    document.querySelector("#TradePage2").style.display = "none";
+    document.querySelector("#BoostPage").style.display = "none";
+    document.querySelector("#WalletPage").style.display = "none";
+
+    document.querySelector("#cGet-info").innerHTML = "<img width='32px' height='32px' src='./TradePageIcons/ETH.png' alt=''><div style='font-size: 18px;margin-left: 10px;'>ETH</div>";
 }
 
 
