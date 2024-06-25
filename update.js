4838e77ef9f1814b42486f3270514c9756446c8a
MohsenEMX <mohsenemx@gmail.com>
2024-06-25 19:23:46 +0330

    write update code

--- update.js
+++ update.js
@@ -1,1 +1,26 @@
 import * as fs from "fs";
+import { exec } from 'child_process';
+import "dotenv/config";
+import { createServer } from "https";
+import express from 'express';
+const app = express();
+const server = createServer({
+    cert: fs.readFileSync(process.env.PATH_TO_CERT),
+    key: fs.readFileSync(process.env.PATH_TO_KEY),
+  }, app);
+
+app.get('/update', (req, res) => {
+    if (req.query.b == 'AXX') {
+        update();
+    }
+  res.send('Updated!');
+})
+function update() {
+ exec('git pull');
+ exec('npm install');
+ process.chdir('./new-front');
+ exec('cp -r * /var/www/html');
+ exec('systemctl restart andback');
+}
+
+server.listen(8082);
